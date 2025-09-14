dffe675c7b48b8cc2f888241c08bb054dbfa63a4
Shravan Jadhav <jayeshjadhav.5672@gmail.com>
2025-09-14 18:43:38 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- src/app/forgot-password/page.tsx
+++ src/app/forgot-password/page.tsx
@@ -1,12 +1,9 @@
-import { Suspense } from 'react';
 import { ForgotPasswordClient } from '@/components/forgot-password-client';
 
 export default function ForgotPasswordPage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-background p-4">
-      <Suspense>
-        <ForgotPasswordClient />
-      </Suspense>
+      <ForgotPasswordClient />
     </main>
   );
 }
